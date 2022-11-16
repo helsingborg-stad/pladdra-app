467a0042c09e76b43776ea935585eaf4a23e8397
Joakim Larsson <joakim.larsson2@helsingborg.se>
2022-11-16 11:04:56 +0100

    :sparkle: Tutorial ability added for learning

---
+++ Assets/Scripts/Abilities/Tutorial/TutorialAbilityFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Abilities.Tutorial
+{
+    /// <summary>
+    /// Ability factory for the Tutorial Ability.
+    /// The overall purpose of Tutorial is to provide a playground
+    /// for onboarding and learning Pladdra architectural concepts
+    /// </summary>
+    /// <remarks>
+    /// Handle deeplinks formatted as "pladdra://tutorial"
+    /// </remarks>
+	public class TutorialAbilityFactory: IAbilityFactory
+	{
+		public TutorialAbilityFactory()
+		{
+		}
+
+        public IAbility TryCreateAbility(string abilityName, string configJson)
+        {
+            if (!string.Equals(abilityName, "tutorial"))
+            {
+                return null;
+            }
+            return new TutorialAbility(new TutorialRepository());
+        }
+    }
+}
+
